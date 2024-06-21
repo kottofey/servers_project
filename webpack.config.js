f9127b75446980d37dfeea0f1a85d7549e5402d5
Kottofey (BigBrother) <romeokontakt@gmail.com>
2024-06-21 03:13:29 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	mode: 'development',
+	entry: {
+		index: ['@babel/polyfill', './scripts/index.js'],
+	},
+	output: {
+		filename: './scripts/[name].[contenthash].js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	devtool: 'source-map',
+	plugins: [
+		new htmlWebpackPlugin({
+			template: './index.html',
+		}),
+		new htmlWebpackPlugin({
+			filename: 'edo.html',
+			template: './edo.html',
+		}),
+		new htmlWebpackPlugin({
+			filename: 'is_mail.html',
+			template: './is_mail.html',
+		}),
+		new htmlWebpackPlugin({
+			filename: 'vm_list.html',
+			template: './vm_list.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'styles/style.css',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(?:js|mjs|cjs)$/,
+				include: path.resolve(__dirname, 'src/scripts'),
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [['@babel/preset-env', { targets: 'defaults' }]],
+					},
+				},
+			},
+			{
+				test: /\.(scss|sass|css)$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(html?)$/i,
+				use: 'html-loader',
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'images/[name]-[contenthash][ext]',
+				},
+			},
+			{
+				test: /\.(woff|ttf|eot|woff2)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'fonts/[name]-[contenthash][ext]',
+				},
+			},
+		],
+	},
+	devServer: {
+		port: 4200,
+	},
+};
